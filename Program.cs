7f438bd8ac1492c2ff219f5ae10c68914ffec85a
horace saturday <horacewillie7@gmail.com>
2021-11-10 09:02:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TimeLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "c://Assignment//LogFile.txt";
+            File.Create(path);
+            Stack<DateTime> time = new Stack<DateTime>();
+            time.Push(DateTime.Now);
+            Thread.Sleep(10000);
+
+            time.Push(DateTime.Now);
+            Console.WriteLine(time.Peek());
+            Thread.Sleep(10000);
+
+            time.Push(DateTime.Now);
+            Console.WriteLine(time.Peek());
+            Thread.Sleep(10000);
+
+            
+
+            foreach (var second in time)
+            {
+                File.AppendAllLines(path, new String[] { second.ToString() });
+            }
+
+            // Console.WriteLine(DateTime.Now.ToString("hh:mm:ss"));
+            //Thread.Sleep(5000);
+        }
+    }
+}
